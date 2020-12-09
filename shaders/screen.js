5b75be683d04604fa96165370f7af48d7f3f511e
Florian Morel <ayamflow@gmail.com>
2020-12-09 18:51:45 +0100

    screen: use Shader default code

--- shaders/screen.js
+++ shaders/screen.js
@@ -17,17 +17,10 @@ export class ScreenShader extends Shader {
     constructor(options = {}) {
         super({
             vertexShader: Shader.quadVertexShader,
-            fragmentShader: `
-                uniform sampler2D tMap;
-                uniform vec2 resolution;
-                varying vec2 vUv;
-                void main() {
-                    gl_FragColor = texture2D(tMap, vUv);
-                    gl_FragColor.a = 1.0;
-                }`,
+            fragmentShader: Shader.quadFragmentShader,
             uniforms: {
                 tMap: options.tMap || { value: null },
-                resolution: Uniforms.resolution
+                uAlpha: options.uAlpha || { value: 1 }
             },
             // depthTest: options.depthTest,
             // depthWrite: options.depthWrite,
